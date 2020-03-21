1673a65c46eb685745f61ba580c6f585bbe664e8
stst5821 <stst5821@gmail.com>
2020-03-22 00:23:46 +0900

    mainをカルーセル化

---
+++ javascript.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper('.swiper-container', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+});
