7a8b3ef9136c1329650e7872976bad2f789969f6
stst5821 <stst5821@gmail.com>
2020-03-25 23:56:53 +0900

    スムーススクロール実装

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 // カルーセル
+
 var swiper = new Swiper('.swiper-container', {
   navigation: {
     nextEl: '.swiper-button-next',
@@ -13,10 +14,35 @@ var swiper = new Swiper('.swiper-container', {
 });
 
 // フワッとカードを表示させる
+
 new WOW().init();
 
-// スマホ用スライドメニュー
-// $(".offcanvas-left").hiraku({
-//   btn:"#offcanvas-btn-left",
-//   direction:"left"
-// });
+// TOPへ戻るボタン 画面右下の
+
+jQuery(window).on("scroll", function($) {
+  if (jQuery(this).scrollTop() > 100) {
+    jQuery('.floating').show();
+  } else {
+    jQuery('.floating').hide();
+  }
+});
+
+jQuery('.floating').click(function () {
+  jQuery('body,html').animate({
+    scrollTop: 0
+  }, 500);
+  return false;
+});
+
+// ヘッダーメニューのスムーススクロール
+
+$(function () {
+  var headerHight = 100; // ヘッダの高さを指定する
+  $('.nav_btn').click(function(){
+      var href= $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+         var position = target.offset().top-headerHight; //ヘッダの高さ分位置をずらす
+      $("html, body").animate({scrollTop:position}, 550, "swing");
+         return false;
+    });
+ });
