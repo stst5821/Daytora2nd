a06a8308dace95eba6d4f0c6ab8f3f60a7b35734
stst5821 <stst5821@gmail.com>
2020-04-01 23:43:54 +0900

    ナビメニューを選ぶと下線が表示

--- javascript.js
+++ javascript.js
@@ -19,7 +19,7 @@ new WOW().init();
 
 // TOPへ戻るボタン 画面右下の
 
-jQuery(window).on("scroll", function($) {
+jQuery(window).on("scroll", function ($) {
   if (jQuery(this).scrollTop() > 100) {
     jQuery('.floating').show();
   } else {
@@ -35,14 +35,22 @@ jQuery('.floating').click(function () {
 });
 
 // ヘッダーメニューのスムーススクロール
+var headerHight = 100; // ヘッダの高さを指定する
+$('.nav_btn').click(function () {
+  var href = $(this).attr("href");
+  var target = $(href == "#" || href == "" ? 'html' : href);
+  var position = target.offset().top - headerHight; //ヘッダの高さ分位置をずらす
+  $("html, body").animate({ scrollTop: position }, 550, "swing");
+});
 
-$(function () {
-  var headerHight = 100; // ヘッダの高さを指定する
-  $('.nav_btn').click(function(){
-      var href= $(this).attr("href");
-        var target = $(href == "#" || href == "" ? 'html' : href);
-         var position = target.offset().top-headerHight; //ヘッダの高さ分位置をずらす
-      $("html, body").animate({scrollTop:position}, 550, "swing");
-         return false;
-    });
- });
+// ヘッダーメニューをクリックしたら下線を引く
+$('.nav_item').click(function () {
+  var $answer = $(this).find('.nav_btn');
+
+  if ($answer.hasClass('active')) {
+    $answer.removeClass('active');
+  } else {
+    $('.nav_btn').removeClass('active');
+    $answer.addClass('active');
+  }
+});
